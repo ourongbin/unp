e41696ddcde7f293adbc05f0e24453ce7981cf14
ourongbin <ourongbin@meituan.com>
2017-04-25 11:06:43 +0800

    echo server v4: 基于epoll，单线程Reactor

--- inc/unpbase.h
+++ inc/unpbase.h
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/epoll.h>
 
 #include "error.h"
 
--- src/echoserv.c
+++ src/echoserv.c
@@ -1,59 +1,75 @@
 #include "unpbase.h"
 #include "sockutil.h"
 
-void * doit(void *arg) {
-    if (pthread_detach(pthread_self()) < 0) {
-        error("pthread_detach");
-    }
-    int fd = (int)arg;
-    char buff[MAXLINE];
-    int  n;
-    for (;;) {
-        n = read(fd, buff, MAXLINE);
-        if (n < 0 && errno == EINTR) {
-            continue;
-        } else if (n <= 0) {
-            break;
-        } 
-        //printf("Recvd %s", buff);
+#define MAXEVENTS 10
 
-        if (writen(fd, buff, strlen(buff)) < strlen(buff)) {
-            error("write short");
-        }
+int doit(int fd) {
+    char buf[MAXLINE];
+
+    int n = read(fd, buf, MAXLINE);
+    if (n <= 0) {
+        return -1;
     }
-    printf("Close connection\n");
-    if (close(fd) < 0) {
-        error_sys("close fd %d", fd);
+    if (writen(fd, buf, n) != n) {
+        error_sys("writen");
+        return -1;
     }
-    return NULL;
+
+    return 0;
 }
 
 int main(int argc, const char * argv[]) {
     int port = 1234;
-    char * portEnv;
+    char *portEnv;
     if (argc > 1) {
         port = atoi(argv[1]);
     } else if ( (portEnv = getenv("LISTENP")) != NULL) {
         port = atoi(portEnv);
     }
 
+    int serv_fd = open_listen(port);
 
-    int sfd = open_listen(port);
-    
+    int epoll_fd = epoll_create(0xCAFE); // the param is ignored
+    if (epoll_fd < 0 ) {
+        fatal_sys("epoll_create");
+    }
+
+    struct epoll_event ev, events[MAXEVENTS];
+    ev.events = EPOLLIN;
+    ev.data.fd = serv_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, serv_fd, &ev) < 0) {
+        fatal_sys("epoll_ctl add serv");
+    }
+
     for (;;) {
-        struct sockaddr_in peer;
-        socklen_t len = sizeof(peer);
-        char ip[16];
-        int fd = accept(sfd, (SA *)&peer, &len);
-        if (fd < 0) {
-            fatal_sys("accept:");
-        } else {
-            printf("New connection: %s:%d\n", inet_ntop(AF_INET, (void *)&peer.sin_addr, ip, sizeof(ip)), ntohs(peer.sin_port));
+        int nfds = epoll_wait(epoll_fd, events, MAXEVENTS, -1);
+        if (nfds < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            fatal_sys("epoll_wait");
         }
 
-        pthread_t th;
-        if (pthread_create(&th, NULL, doit, (void *)fd) < 0) {
-            error("pthread_create");
+        int i = 0;
+        for (; i < nfds; i++) {
+            if (events[i].data.fd == serv_fd) {
+                int conn_fd = accept(serv_fd, NULL, NULL);
+                if (conn_fd < 0) {
+                    fatal_sys("accept");
+                }
+                ev.events = EPOLLIN;
+                ev.data.fd = conn_fd;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn_fd, &ev) < 0) {
+                    fatal_sys("epoll_ctl add conn");
+                }
+            } else {
+                if (doit(events[i].data.fd) < 0) {
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL) < 0) {
+                        fatal_sys("epoll_ctl del conn");
+                    }
+                    close(events[i].data.fd);
+                }
+            }
         }
     }
 
